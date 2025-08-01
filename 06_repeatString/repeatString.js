5a2cdcf70db7d65b12a8085bc5622bad3dc853c7
hqluke <212039062+hqluke@users.noreply.github.com>
2025-08-01 04:26:33 -0600

    06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = ((word, num) => {
+    if(num <= -1){
+        return word = "ERROR"
+    }
+    let i = 0;
+    word1 = ""
+    if(num == 0){return word1;}
+    while (i < num){
+        word1 += word;
+        i++;
+    }
+    return word1;
+});
 
-};
+
 
 // Do not edit below this line
 module.exports = repeatString;
