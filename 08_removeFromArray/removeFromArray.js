f8646b11cbf8017933239952606e3f432566fef1
lukepeterson1 <212039062+hqluke@users.noreply.github.com>
2025-08-11 17:45:09 -0600

    08, 09 (forgot to commit gahhh)

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+    return arr.filter( a => !remove.includes(a));
+    
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(...nums) {
+    
+    
+    a = nums[0];
+    b = nums[nums.length-1];
+    arr = [];
+    total = 0;
+    if(a < 0 || b < 0 || Number.isInteger(a) == false || Number.isInteger(b) == false){
+        return "ERROR";
+    }
+    if( a > b){
+        for (b; b < a + 1; b++){
+        arr.push(b);
+        }
+    }
+    else {
+        for (a; a < b + 1; a++){
+            arr.push(a);
+        }
+    }
+    
+    for(num of arr){
+        total += num;
+    }
+    return total;
 };
 
 // Do not edit below this line
